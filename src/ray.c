6b95050cf08d7670760d5a3cc2da6cea1a02e1c0
N.Arai <chotto2@mac.com>
2026-04-12 05:25:08 +0900

    Setting the upper limit of an integer by the first argument.

--- src/ray.c
+++ src/ray.c
@@ -42,11 +42,17 @@
 #include <string.h>
 #include <sys/resource.h>
 #include <sys/time.h>
+#include <ctype.h>
 
-#define X_MAX (4000000)	
+//#define X_MAX (4000000)
+#define X_MAX x_max
 #define Y_MAX (X_MAX)
 #define A_MAX (X_MAX)
 #define DSP_MAX (128)
+
+#define ERR_ARGSTYPE   (-1)
+#define ERR_DIVSALOC   (-2)
+#define ERR_POOLALOC   (-3)
 
 uint32_t *divs_pool;
 
@@ -57,7 +63,16 @@ typedef struct {
 } DIVS, *pDIVS;
 pDIVS divs;
 
+uint64_t x_max = 1000000;
 int benchmark_mode = 0;
+
+int is_digits(const char *s) {
+	if (s == NULL || *s == '\0') return 0;
+	for (; *s; s++) {
+		if (!isdigit((unsigned char)*s)) return 0;
+	}
+	return 1;
+}
 
 /**
  * @brief Main entry point
@@ -76,16 +91,27 @@ int32_t main(int argc, char *argv[])
 
 	/*--- check argv ---*/
 	for (int i = 1; i < argc; i++) {
-    		if (strcmp(argv[i], "--benchmark") == 0) {
-        		benchmark_mode = 1;
- 		}
+		if (strcmp(argv[i], "--benchmark") == 0) {
+			benchmark_mode = 1;
+		}
+		else {
+			if (i == 1) {
+				if (is_digits(argv[i])) {
+					x_max = atoll(argv[i]);
+				}
+				else {
+					printf("USAGE: ray [<n_max>] [--benchmark]\n");
+					return ERR_ARGSTYPE;
+				}
+			}
+		}
 	}
 
 	/*--- alloc divs ---*/
 	divs = calloc(X_MAX+1, sizeof(DIVS));
 	if (divs == NULL) {
-		printf("ERR: divs(0) = calloc(%d, %ld)\n", X_MAX+1, sizeof(DIVS));
-		return -1;
+		printf("ERR: divs(0) = calloc(%ld, %ld)\n", X_MAX+1, sizeof(DIVS));
+		return ERR_DIVSALOC;
 	}
 
 	/*--- make pool_cnt ---*/
@@ -106,8 +132,10 @@ int32_t main(int argc, char *argv[])
 	/*--- alloc pool ---*/
 	divs_pool = calloc(ofs, sizeof(uint32_t));
 	if (divs_pool == NULL) {
+		free(divs);
+		divs = NULL;
 		printf("ERR: divs_pool(0) = calloc(%d, %ld)\n", ofs, sizeof(uint32_t));
-		return -2;
+		return ERR_POOLALOC;
 	}
 
 	/*--- get start time ---*/
@@ -146,7 +174,7 @@ int32_t main(int argc, char *argv[])
 	else {
 		/*--- print divisor stars ---*/
 		printf("      n:   d(n):divisors2(n, %d)\n", DSP_MAX);
-		printf("%7d:%7d:", 0, Y_MAX);
+		printf("%7d:%7ld:", 0, Y_MAX);
 		for (y = 1; y <= DSP_MAX; y++) {
 			printf("*");
 		}
